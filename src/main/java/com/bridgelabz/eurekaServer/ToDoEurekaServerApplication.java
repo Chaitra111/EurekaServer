68c115592fc31dfb1f842401d613ecbffe9fbdc2
Chaitra111 <111chaitra@gmail.com>
2018-08-21 14:41:42 +0530

    eurekaserver

--- src/main/java/com/bridgelabz/eurekaServer/ToDoEurekaServerApplication.java
+++ src/main/java/com/bridgelabz/eurekaServer/ToDoEurekaServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author Chaitra Ankolekar
+ * Purpose :Eureka Server where every microservice will register
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class ToDoEurekaServerApplication {
